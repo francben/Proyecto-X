ba91c76c6b54fcb94e73ffb159de9905a52c73b1
mel <meleve95@gmail.com>
2016-05-08 00:27:13 -0400

    modif

--- Proyexto X/js/RobotXero.js
+++ Proyexto X/js/RobotXero.js
@@ -4,165 +4,191 @@ function RobotXero(x, y, w, h){
 	this.w = w;
 	this.h = h;
 
+	var med = 8.0;
+
+
 	this.dibu = function(ctx){
 		//la cara
 		//ctx.strokeStyle = "black";
 		//ctx.fillStyle = "black";
-
+	
 		//Cabeza
-		ctx.strokeRect(this.x + 3/8*this.w, this.y + 2/8*this.h, 2/8.0*this.w , 1/8.0*this.h  );
+		ctx.fillStyle= "black";
+		ctx.fillRect (this.x + 3/med*this.w, this.y + 2/med*this.h, 1/med*this.w , 1/med*this.h);
+
 		//orejas
-		ctx.fillStyle = "black";
-		ctx.fillRect(this.x + 2.9/8*this.w, this.y + 2.2/8*this.h, 0.1/8.0*this.w , 0.5/8.0*this.h  );
-		ctx.fillRect(this.x + 5/8*this.w, this.y + 2.2/8*this.h, 0.1/8.0*this.w , 0.5/8.0*this.h  );
+		ctx.fillStyle = "white";
+		ctx.fillRect(this.x + 3.4/med*this.w, this.y + 2.2/med*this.h, 0.1/med*this.w , 0.3/med*this.h);
+		//ctx.fillRect(this.x + 5/med*this.w, this.y + 2.2/med*this.h, 0.1/med*this.w , 0.5/med*this.h);
 
 		//antena
-		ctx.strokeRect(this.x + 3.7/8*this.w, this.y + 1.8/8*this.h, 0.5/8.0*this.w , 0.2/8.0*this.h);
-		ctx.strokeRect(this.x + 3.8/8*this.w, this.y + 1.7/8*this.h, 0.3/8.0*this.w , 0.1/8.0*this.h);
-		ctx.strokeRect(this.x + 3.9/8*this.w, this.y + 1.4/8*this.h, 0.1/8.0*this.w , 0.3/8.0*this.h);
+		ctx.fillStyle= "grey";
+		ctx.fillRect(this.x + 3.5/med*this.w, this.y + 1.8/med*this.h, 0.2/med*this.w , 0.2/med*this.h);
+		ctx.strokeStyle= "red";
+		ctx.strokeRect(this.x + 3.5/med*this.w, this.y + 1.7/med*this.h, 0.1/med*this.w , 0.1/med*this.h);
+		ctx.fillStyle= "grey";
+		ctx.fillRect(this.x + 3.5/med*this.w, this.y + 1.4/med*this.h, 0.1/med*this.w , 0.3/med*this.h);
 
 		ctx.fillStyle = "green";
 		ctx.beginPath();
-		ctx.arc(this.x + 3.9/8*this.w, this.y + 1.3/8*this.h, 0.1/8*this.h, 0, Math.PI*2, true);
+		ctx.arc(this.x + 3.5/med*this.w, this.y + 1.3/med*this.h, 0.1/med*this.h, 0, Math.PI*2, true);
 		ctx.closePath();
 		ctx.fill();
 		
 		//cuello
 		ctx.fillStyle = "black";
-		ctx.fillRect(this.x + 3.7/8*this.w, this.y + 3/8*this.h, 0.5/8.0*this.w , 1/8.0*this.h);
+		ctx.fillRect(this.x + 3.5/med*this.w, this.y + 3/med*this.h, 0.2/med*this.w , 1/med*this.h);
 		
 		//pansa
-		ctx.strokeRect(this.x + 2.8/8*this.w, this.y + 4/8*this.h, 2.5/8.0*this.w , 2.5/8.0*this.h);
+		ctx.fillStyle= "grey";
+		ctx.fillRect(this.x + 2.8/med*this.w, this.y + 4/med*this.h, 1.5/med*this.w , 2.5/med*this.h);
 
-		ctx.fillStyle = "black";
+		/*ctx.fillStyle = "black";
 		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 5/8*this.w, this.y + 5/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 4/8*this.w, this.y + 4/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 3/8*this.w, this.y + 5/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 4/8*this.w, this.y + 6.5/8*this.h); // Set the path destination
+		ctx.moveTo(this.x + 3/med*this.w, this.y + 4/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 7.7/med*this.w, this.y + 6/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 7/med*this.w, this.y + 5/med*this.h); // Set the path destination
+		//ctx.lineTo(this.x + 5/med*this.w, this.y + 6.5/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
-		ctx.fill(); // Outline the path
+		ctx.fill(); // Outline the path*/
 
 		//Braso 1
-		ctx.strokeRect(this.x + 1.8/8*this.w, this.y + 4.5/8*this.h, 1/8.0*this.w , 0.5/8.0*this.h);
+		ctx.fillStyle= "grey";
+		//ctx.strokeStyle= "black";
+		ctx.fillRect(this.x + 4.0/med*this.w, this.y + 5.0/med*this.h, 1/med*this.w , 0.5/med*this.h);
+		ctx.fill();
 
 		ctx.fillStyle = "red";
 		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 1.8/8*this.w, this.y + 5.0/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 1.5/8*this.w,this.y + 5/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 1.8/8*this.w,this.y + 4.7/8*this.h); // Set the path destination
+		ctx.moveTo(this.x + 5.0/med*this.w, this.y + 5.00/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 5.0/med*this.w,this.y + 5.40/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 5.3/med*this.w,this.y + 5.02/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
 		ctx.fill();
 
 		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 1.8/8*this.w, this.y + 4.5/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 1.5/8*this.w,this.y + 4.5/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 1.8/8*this.w,this.y + 4.7/8*this.h); // Set the path destination
+		ctx.moveTo(this.x + 5.45/med*this.w, this.y + 5.47/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 5.00/med*this.w,this.y + 5.00/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 5.00/med*this.w,this.y + 5.47/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
 		ctx.fill();
 
 		//Braso 2
-		ctx.strokeRect(this.x + 5.3/8*this.w, this.y + 4.5/8*this.h, 1/8.0*this.w , 0.5/8.0*this.h);
+		ctx.fillStyle= "grey";
+		ctx.fillRect(this.x + 4.3/med*this.w, this.y + 4.5/med*this.h, 0.50/med*this.w , 0.4/med*this.h);
 
 		ctx.fillStyle = "red";
 		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 6.3/8*this.w, this.y + 5.0/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 6.3/8*this.w,this.y + 4.8/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 6.6/8*this.w,this.y + 5.0/8*this.h); // Set the path destination
+		ctx.moveTo(this.x + 4.7/med*this.w, this.y + 4.90/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 4.7/med*this.w,this.y + 4.7/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 5.0/med*this.w,this.y + 4.95/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
 		ctx.fill();
 
 		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 6.3/8*this.w, this.y + 4.5/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 6.3/8*this.w,this.y + 4.8/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 6.6/8*this.w,this.y + 4.5/8*this.h); // Set the path destination
+		ctx.moveTo(this.x + 4.7/med*this.w, this.y + 4.5/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 4.7/med*this.w,this.y + 4.8/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 5.0/med*this.w,this.y + 4.5/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
 		ctx.fill();
 
+		/*ctx.beginPath(); // Start the path
+		ctx.moveTo(8/med, 450/med); // Set the path origin
+		ctx.lineTo(8/med, 450/med); // Set the path destination
+		ctx.closePath(); // Close the path
+		ctx.stroke(); // Outline the path*/
+
 
 		//PIERNAs
-		ctx.strokeRect(this.x + 3.3/8*this.w, this.y + 6.5/8*this.h, 0.5/8.0*this.w , 1/8.0*this.h);
-		ctx.strokeRect(this.x + 4.3/8*this.w, this.y + 6.5/8*this.h, 0.5/8.0*this.w , 1/8.0*this.h);
+		ctx.fillStyle= "grey";
+		ctx.fillRect(this.x + 3.2/med*this.w, this.y + 6.5/med*this.h, 0.3/med*this.w , 1/med*this.h);
+		ctx.fillRect(this.x + 3.7/med*this.w, this.y + 6.5/med*this.h, 0.3/med*this.w , 1/med*this.h);
+
 		//pies
+		ctx.fillStyle= "grey";
+		//ctx.strokeStyle= "red";
+		ctx.beginPath();
+		ctx.arc(this.x + 3.4/med*this.w, this.y + 7.5/med*this.h, 0.2/med*this.h, 0, Math.PI*1, false);
+		ctx.closePath();
+		ctx.fill();
+		//ctx.stroke();
 		
 		ctx.beginPath();
-		ctx.arc(this.x + 3.5/8*this.w, this.y + 7.5/8*this.h, 0.2/8*this.h, 0, Math.PI*2, true);
+		ctx.arc(this.x + 3.9/med*this.w, this.y + 7.5/med*this.h, 0.2/med*this.h, 0, Math.PI*1, false);
 		ctx.closePath();
-		ctx.stroke();
-		
-		ctx.beginPath();
-		ctx.arc(this.x + 4.6/8*this.w, this.y + 7.5/8*this.h, 0.2/8*this.h, 0, Math.PI*2, true);
-		ctx.closePath();
-		ctx.stroke();
+		ctx.fill();
+		//ctx.stroke();
 
 		//ojos
 		ctx.fillStyle= "#ff0000";
 		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 3.3/8*this.w, this.y + 2.1/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 3.2/8*this.w,this.y + 2.3/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 3.6/8*this.w,this.y + 2.4/8*this.h); // Set the path destination
+		ctx.moveTo(this.x + 3.7/med*this.w, this.y + 2.2/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 3.6/med*this.w,this.y + 2.3/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 4.0/med*this.w,this.y + 2.4/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
 		ctx.fill();
 
-		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 4.7/8*this.w, this.y + 2.1/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 4.4/8*this.w,this.y + 2.4/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 4.8/8*this.w,this.y + 2.3/8*this.h); // Set the path destination
+		/*ctx.beginPath(); // Start the path
+		ctx.moveTo(this.x + 4.7/med*this.w, this.y + 2.1/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 4.4/med*this.w,this.y + 2.4/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 4.8/med*this.w,this.y + 2.3/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
-		ctx.fill();
+		ctx.fill();*/
 
 		//boca 
-		ctx.strokeRect(this.x + 3.3/8*this.w, this.y + 2.6/8*this.h, 1.4/8.0*this.w , 0.3/8.0*this.h  );
+		ctx.strokeStyle= "red";
+		ctx.strokeRect(this.x + 3.6/med*this.w, this.y + 2.6/med*this.h, 0.4/med*this.w , 0.3/med*this.h  );
 
 		//dientes
 		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 3.5/8*this.w, this.y + 2.6/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 3.3/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 3.5/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
+		ctx.moveTo(this.x + 3.8/med*this.w, this.y + 2.6/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 3.6/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 3.8/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
 		ctx.stroke();
 		
 		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 3.7/8*this.w, this.y + 2.6/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 3.5/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 3.7/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
+		ctx.moveTo(this.x + 4.0/med*this.w, this.y + 2.6/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 3.8/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 4.0/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
 		ctx.stroke();
 
-		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 3.9/8*this.w, this.y + 2.6/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 3.7/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 3.9/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
+		/*ctx.beginPath(); // Start the path
+		ctx.moveTo(this.x + 3.9/med*this.w, this.y + 2.6/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 3.7/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 3.9/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
 		ctx.stroke();
 		
 		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 4.1/8*this.w, this.y + 2.6/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 3.9/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 4.1/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
+		ctx.moveTo(this.x + 4.1/med*this.w, this.y + 2.6/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 3.9/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 4.1/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
 		ctx.stroke();
 
 		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 4.3/8*this.w, this.y + 2.6/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 4.1/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 4.3/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
+		ctx.moveTo(this.x + 4.3/med*this.w, this.y + 2.6/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 4.1/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 4.3/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
 		ctx.stroke();
 		
 		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 4.5/8*this.w, this.y + 2.6/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 4.3/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 4.5/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
+		ctx.moveTo(this.x + 4.5/med*this.w, this.y + 2.6/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 4.3/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 4.5/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
 		ctx.stroke();
 
 		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x + 4.7/8*this.w, this.y + 2.6/8*this.h); // Set the path origin
-		ctx.lineTo(this.x + 4.5/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
-		ctx.lineTo(this.x + 4.7/8*this.w,this.y + 2.9/8*this.h); // Set the path destination
+		ctx.moveTo(this.x + 4.7/med*this.w, this.y + 2.6/med*this.h); // Set the path origin
+		ctx.lineTo(this.x + 4.5/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
+		ctx.lineTo(this.x + 4.7/med*this.w,this.y + 2.9/med*this.h); // Set the path destination
 		ctx.closePath(); // Close the path
-		ctx.stroke();
+		ctx.stroke();*/
+
 
 	}
 }
