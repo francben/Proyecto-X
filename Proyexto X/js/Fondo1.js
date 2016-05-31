2afb74fa86b556fb9168e5c4cc64586bfe697cdf
Cris <helloocriss@gmail.com>
2016-05-30 21:13:13 -0400

    Fondo1 Mejorado

--- Proyexto X/js/Fondo1.js
+++ Proyexto X/js/Fondo1.js
@@ -1,29 +1,14 @@
-function Fondo1(x, y, w, h){
-					this.x = x;
-					this.y = y;
-					this.w = w;
-					this.h = h;
+function Fondo1(){
+
+	this.idElementFondo = "#imgFondo1";
+
 	this.dibujar = function(ctx){
-		// puerta medio
-		ctx.strokeStyle = "black";
-		ctx.beginPath(); // Start the path
-		ctx.moveTo(this.x+0/8*this.w, this.y+0/8*this.h); // Set the path origin
-		ctx.lineTo(this.x+0/8*this.w, this.y+20/8*this.h); // Set the path destination
-		ctx.closePath(); // Close the path
-		ctx.stroke(); // Outline the path
 
+		var f1 = $(this.idElementFondo)[0];
+	console.log("F1",f1);
+		ctx.drawImage(f1,0,0);
 
-	var canvas = document.getElementById("mycanvas");
-	var ctx = canvas.getContext("2d");
-	//context = canvas.get(0).getContext("2d");
-	var fondo1 = new Image();
-	
-	fondo1.src = "img2.jpg";
-		
-
-	fondo1.onload = function(){
-		ctx.drawImage(fondo1,0,0)
 			
-	} 
-
+	
+	}
 }
