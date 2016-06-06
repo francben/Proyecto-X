2b09387c3725a587221804c47445aa6c28e1c1ba
Franc <franczore@gmail.com>
2016-06-05 23:23:59 -0400

    agregue al index los robots para probar, Robot V y el Alpha tambien con el fondo e hice algunas correciones al fondo1

--- Proyexto X/js/Fondo1.js
+++ Proyexto X/js/Fondo1.js
@@ -1,12 +1,15 @@
-function Fondo1(){
+function Fondo1(x,y,w,h){
+	this.x=x;
+	this.y=y;
+	this.w=w;
+	this.h=h;
 
 	this.idElementFondo = "#imgFondo1";
 
 	this.dibujar = function(ctx){
 
 		var f1 = $(this.idElementFondo)[0];
-	console.log("F1",f1);
-		ctx.drawImage(f1,0,0);
+		ctx.drawImage(f1,x,y,w,h);
 
 			
 	
