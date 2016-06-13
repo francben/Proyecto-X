f0b823d4c3b589db1df68e88bc6dd62043029cbe
Franc <franczore@gmail.com>
2016-06-13 19:33:54 -0400

    agregue la bala y creer un arhivo juego para cargar los niveles

---
+++ Proyexto X/js/BalaenErgia.js
@@ -0,0 +1,49 @@
+function circulo(context,x,y,radio,b,ar,p,color){
+	context.save();
+	context.fillStyle= '#'+color;
+	context.beginPath();
+	context.arc(x, y, radio, b, ar, p);
+	context.closePath();
+	context.fill();
+	context.restore();
+	
+}
+function relleno(context,w,h,radio,e,t,a,b,color1,color2){
+	context.save();
+	var gradient = context.createRadialGradient(w/2, h/2, e,w/2, h/2, t);
+	gradient.addColorStop(a, '#'+color1);
+	gradient.addColorStop(a, '#'+color2);
+	context.fillStyle = gradient;
+	context.arc(w, h,radio, 0, Math.PI*2, false);
+	context.closePath();
+	context.fill();
+	context.restore();
+	
+}
+
+function brillo(context,ui,x,y,color){
+	//context.save();
+	context.shadowColor = '#'+color; 
+	context.shadowBlur = ui;
+	context.shadowOffsetX = x;
+	context.shadowOffsetY = y;
+	//context.restore();
+}
+
+function BalaEnergia(x, y, w, h){
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+	this.velocidad = 5;
+	this.dirX = -1;
+	this.dirY = 1;
+	var grilla = 8;
+	this.exploto = false;
+	this.dibujar = function(context){
+				brillo(context,3/grilla*this.w,0,0,'f00');
+	    		//relleno(context,this.x+1/grilla*this.w, this.y+4.5/grilla*this.h, 1.5/grilla*this.w, 1/grilla*this.w, 0.5/grilla*this.w,0,1,'f99','fff');
+				circulo(context, this.x+2/grilla*this.w, this.y+4.5/grilla*this.h, 3/grilla*this.w, 0, Math.PI*2, true,'000');
+				context.restore();
+	};
+}
---
+++ Proyexto X/js/Juego.js
@@ -0,0 +1,17 @@
+var niveles = [];
+var nivel1 = [];
+
+function CargarOjetos(){
+	
+	}
+
+}
+function dibujarfondo(){
+	ctx.drawImage(fondo,0,0);
+}
+
+function frameloop(){
+	dibujarfondo();
+}
+//Ejecucion de funcines
+loadMedia();
